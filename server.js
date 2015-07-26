655c5abd91f0853a0594eb8baf3c101658792c2a
Manuel Moreta <manukorp@outlook.com>
2015-07-26 15:19:36 +0000

    Actualizando server.js agregando lectura de archivos de manera asincrona

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ server.on("request",onRequest)
 server.on("listening", onListening)
 
 function onRequest(req, res){
-	let file = fs.readFileSync('public/index.html')
-	res.end(file)
+	let file = fs.readFile('public/index.html', function(error, file){
+		if(error){
+			return res.end(error.msg)
+		}else{
+			return res.end(file)
+		}
+	})
 }
 
 function onListening(){
